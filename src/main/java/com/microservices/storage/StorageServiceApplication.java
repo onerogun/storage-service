8e0a0c89cc434bf0e8071c05048e8b2ffefc30bb
Ogun <onerogun@gmail.com>
2021-02-14 13:13:53 -0800

    remmove unused code

--- src/main/java/com/microservices/storage/StorageServiceApplication.java
+++ src/main/java/com/microservices/storage/StorageServiceApplication.java
@@ -2,22 +2,13 @@ package com.microservices.storage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 //@EnableEurekaClient
-@EnableDiscoveryClient
 public class StorageServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StorageServiceApplication.class, args);
 	}
 
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
 }
--- src/main/java/com/microservices/storage/service/StorageService.java
+++ src/main/java/com/microservices/storage/service/StorageService.java
@@ -37,8 +37,6 @@ public class StorageService {
     @Autowired
     private SourceChannels sourceChannels;
 
-    @Autowired
-    private RestTemplate restTemplate;
 
 
     /**
